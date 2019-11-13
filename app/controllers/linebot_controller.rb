04533c400387544f121f2645f1a714d3cdad217c
Keishiro <kn11308@gmail.com>
2019-11-13 15:01:14 +0900

    improve

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -13,7 +13,7 @@ class LinebotController < ApplicationController
       error 400 do 'Bad Request' end
     end
     events = client.parse_events_from(body)
-    events.each { |event|
+    events.each do |event|
       case event
         # メッセージが送信された場合の対応（機能①）
       when Line::Bot::Event::Message
@@ -104,7 +104,7 @@ class LinebotController < ApplicationController
         line_id = event['source']['userId']
         User.find_by(line_id: line_id).destroy
       end
-    }
+    end
     head:ok
   end
 
